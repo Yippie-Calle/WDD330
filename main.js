b44de2330cd034e7fbbaa0aa32ee6ccc43743d03
Yippie_Calle <cal12001@byui.edu>
2021-05-06 22:39:53 -0600

    Updated my notes and added week 3

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const links =[
+const links = [{
+        label: "Week1 Notes",
+        url: "/WDD330/week1.html"
+    },
     {
-        label: "Week1 notes",
-        url: "week1/index.html"
+        label: "Week2 Notes",
+        url: "/WDD330/week2.html"
+    },
+    {
+        label: "Week3 Notes",
+        url: "/WDD330/week3.html"
     }
-]
+];
+
+function addToIndex() {
+    for (let i = 0; i < links.length; i++) {
+        var a = document.createElement("a");
+        var linkText = document.createTextNode(links[i].label);
+        a.appendChild(linkText);
+        a.href = links[i].url;
+        var y = document.createElement("LI");
+        y.appendChild(a);
+        document.getElementById("indexList").appendChild(y);
+    };
+}
