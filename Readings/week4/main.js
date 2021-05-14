c0b7de76184f55499bdf2dd78b86989b825c869a
Yippie_Calle <cal12001@byui.edu>
2021-05-14 06:41:36 -0600

    Working on my notes for the class

--- Readings/week4/main.js
+++ Readings/week4/main.js
@@ -8,3 +8,14 @@ function makeHero(event) {
     alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
     return hero;
 }
+hero.realName = form.realName.value; // Enhance for passwords. 
+// This will go trhough the check box and check to see if something was checked and make sure its stored and recorded. 
+hero.powers = [];
+for (let i=0; i < form.powers.length; i++) {
+    if (form.powers[i].checked) {
+        hero.powers.push(form.powers[i].value);
+    }
+}
+// hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value); <--- this will produce the same result as the one above. 
+hero.category = form.category.value; // this checks what was chosen in the Radio Category
+hero.age = form.age.value; // This grabs age of hero. 
