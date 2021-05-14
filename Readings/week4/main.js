4ae4ba9d2074bb3772e93d7b796b3524acec9e57
Yippie_Calle <cal12001@byui.edu>
2021-05-13 22:58:00 -0600

    added more java

--- Readings/week4/main.js
+++ Readings/week4/main.js
@@ -1,2 +1,10 @@
 const form = document.forms['hero'];
 form.addEventListener('submit', makeHero, false);
+
+function makeHero(event) {
+    event.preventDefault(); // prevent the form from being submitted
+    const hero = {}; // create an empty object
+    hero.name = form.heroName.value; // create a name property based on the input field's value
+    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+    return hero;
+}
