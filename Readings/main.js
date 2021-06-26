9d4198649a5d92f8473c5d9d6e1d5b2b206fb52a
Yippie_Calle <cal12001@byui.edu>
2021-06-26 15:26:30 -0600

    added week10
    Quick reading week 10

--- Readings/main.js
+++ Readings/main.js
@@ -1,49 +1,54 @@
-const links = [{
-        label: "Week1 Notes",
-        url: "Readings/week1.html"
-    },
-    {
-        label: "Week2 Notes",
-        url: "Readings/week2.html"
-    },
-    {
-        label: "Week3 Notes",
-        url: "Readings/week3.html"
-    },
-    {
-        label: "Week4 Notes",
-        url: "Readings/week4.html"
-    },
-    {
-        label: "Week5 Notes",
-        url: "Readings/week5.html"
-    },
-    {
-        label: "Week6 TODO",
-        url: "Readings/week6/week6.html"
-    },
-    {
-        label: "Week7 Notes",
-        url: "Readings/week7.html"
-    },
-    {
-        label: "Week8 Notes",
-        url: "Readings/week8.html"
-    },
-    {
-        label: "Week9 Notes",
-        url: "Readings/week9.html"
-    }
+const links = [
+  {
+    label: "Week1 Notes",
+    url: "Readings/week1.html",
+  },
+  {
+    label: "Week2 Notes",
+    url: "Readings/week2.html",
+  },
+  {
+    label: "Week3 Notes",
+    url: "Readings/week3.html",
+  },
+  {
+    label: "Week4 Notes",
+    url: "Readings/week4.html",
+  },
+  {
+    label: "Week5 Notes",
+    url: "Readings/week5.html",
+  },
+  {
+    label: "Week6 TODO",
+    url: "Readings/week6/week6.html",
+  },
+  {
+    label: "Week7 Notes",
+    url: "Readings/week7.html",
+  },
+  {
+    label: "Week8 Notes",
+    url: "Readings/week8.html",
+  },
+  {
+    label: "Week9 Notes",
+    url: "Readings/week9.html",
+  },
+  {
+    label: "Week10 Notes",
+    url: "Readings/week10.html",
+  },
 ];
 
 function addToIndex() {
-    for (let i = 0; i < links.length; i++) {
-        var a = document.createElement("a");
-        var linkText = document.createTextNode(links[i].label);
-        a.appendChild(linkText);
-        a.href = links[i].url;
-        var y = document.createElement("LI");
-        y.appendChild(a);
-        document.getElementById("indexList").appendChild(y);
-    };
+  for (let i = 0; i < links.length; i++) {
+    var a = document.createElement("a");
+    var linkText = document.createTextNode(links[i].label);
+    a.appendChild(linkText);
+    a.href = links[i].url;
+    var y = document.createElement("LI");
+    y.appendChild(a);
+    document.getElementById("indexList").appendChild(y);
+  }
 }
